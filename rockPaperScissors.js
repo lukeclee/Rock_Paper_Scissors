d25203600582658ad1cd6645db957b6672ee31d4
Luke <lukelee_@outlook.com>
2020-10-27 18:21:25 -0600

    Add UI and refactor code

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -59,19 +59,36 @@ function playRound(playerSelection, computerSelection){
     return result;
 }
 
+function results(){
+    if(computerScore === playerScore){
+        return "It's currently a tie! Both players have a score of " + playerScore + ".";
+    }else if(computerScore > playerScore){
+        return "The computer player is currently winning! They have a score of " + computerScore + ", while you have a score of " + playerScore + ".";
+    }else if(computerScore < playerScore){
+        return "You are currently winning with a score of " + playerScore + " and the computer player has a score of " + computerScore + ".";
+    }
+}
+
 function game(){
     computerScore = 0;
     playerScore = 0;
-    let playerSelection = "";
-    for(i = 0; i < 5; i++){
-        playerSelection = prompt("Please choose rock, paper, or scissors");
-        console.log(playRound(playerSelection.toLowerCase(), computerPlay()));
-    }
-    if(computerScore === playerScore){
-        console.log("It's a tie! Both players had a score of " + playerScore + ".");
-    }else if(computerScore > playerScore){
-        console.log("The computer player won! They had a score of " + computerScore + ", while you had a score of " + playerScore + ".");
-    }else if(computerScore < playerScore){
-        console.log("Congratulations! You won with a score of " + playerScore + " and the computer player had a score of " + computerScore + ".")
-    }
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
+    const roundWinner = document.querySelector('#roundWinner');
+    const currentWinner = document.querySelector('#currentWinner');
+    rock.addEventListener('click', function(e){
+        roundWinner.innerText = playRound('rock', computerPlay());
+        currentWinner.innerText = results();
+    });
+    paper.addEventListener('click', function(e) {
+        roundWinner.innerText = playRound('paper', computerPlay());
+        currentWinner.innerText = results();
+    });
+    scissors.addEventListener('click', function(e) {
+        roundWinner.innerText = playRound('paper', computerPlay());
+        currentWinner.innerText = results();
+    });
 }
+
+game();
